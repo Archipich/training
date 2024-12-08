765a6511c1013cb9825952d136640c6be4e7f5ca
Archipich <footboll2051@gmail.com>
2024-12-08 20:43:22 +0300

    Add files via upload

---
+++ Homework assignment  Variables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerMoney;
+            ushort plyerEnerge;
+            string welcomeMesseng;
+            bool doorIsOpen;
+            float playerSpeed;
+            int enemeyHealth;
+            ushort enemeyMassiv;
+            string fightTraining;
+            bool lampIsLit;
+            float enemeySpeed;
+            playerMoney = 1000;
+            plyerEnerge = 20;
+            welcomeMesseng = "Добро пожаловать в скайрим";
+            doorIsOpen = true;
+            playerSpeed = 10.2f;
+            enemeyHealth = 2000;
+            enemeyMassiv = 100;
+            fightTraining = "подойдите к лучнику чтобы начать тренировку";
+            lampIsLit = false;
+            enemeySpeed = 3.2f;
+
+            
+        }
+    }
+}
