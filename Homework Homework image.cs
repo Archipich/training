0e039b0220e0eb256241ded78f52079cb6b2277e
Archipich <footboll2051@gmail.com>
2024-12-08 23:14:42 +0300

    Add files via upload

---
+++ Homework Homework image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework_image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowLenght = 3;
+            int numberOfImage = 52;
+            int fullLines = numberOfImage / rowLenght;
+            int remainsImage = numberOfImage - (fullLines * 3);
+            Console.WriteLine($"Заполненых рядов {fullLines}, остаток картинок {remainsImage}");
+            Console.ReadKey();
+        }
+    }
+}
