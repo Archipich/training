ae05382c51e8cac5fdb783f0450249225ad51850
Archipich <footboll2051@gmail.com>
2024-12-21 23:58:54 +0300

    homework swap

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework_swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Иванов";
+            string surname = "Петр";
+            Console.WriteLine($"мы нашли ошибку, на данный момент ваше имя {name}, а фамилия {surname}");
+            string swapName = name;
+            name = surname;
+            surname = swapName;
+            Console.WriteLine($"проверьте данные, ваше имя {name}, фамилия {surname}");
+        }
+    }
+}
