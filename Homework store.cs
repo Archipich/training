b479004c9a9b450a2536ea84035885693d36bb6b
Archipich <footboll2051@gmail.com>
2024-12-24 00:03:41 +0300

    Add files via upload

---
+++ Homework store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystal;
+            int crystalPrice = 100;
+            
+            Console.WriteLine($"Приветствую тебя в магазине кристалов странник, сегодня кристалы по {crystalPrice} золота");
+            Console.Write("Сколько у тебя золота?");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристалов тебе нужно?");
+            crystal = Convert.ToInt32(Console.ReadLine());
+
+           
+            gold -= crystal * crystalPrice;
+            Console.WriteLine($"Вы купили {crystal} кристалов и у вас осталось {gold} золота");
+
+
+        }
+    }
+}
