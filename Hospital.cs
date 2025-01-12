8abc39eaafddf5c1b5dac499656e5a4e70e12e95
Archipich <footboll2051@gmail.com>
2025-01-13 00:59:43 +0300

    Add files via upload

---
+++ Hospital.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reseptionTime = 10;
+            int minInHouer = 60;
+            int min;
+            int houre;
+
+            Console.Write("Сколько человек перед вами в очереди: ");
+
+            int queue = Convert.ToInt32(Console.ReadLine());
+            int waitingMinutes = queue * reseptionTime;
+
+            houre = waitingMinutes / minInHouer;
+            min = waitingMinutes % minInHouer;
+
+            Console.WriteLine($"Вам осталось ждать {houre} часов и {min} минут");
+
+
+        }
+    }
+}
