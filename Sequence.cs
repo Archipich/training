993f375155ee065895dbc391b8616c662847ce8d
Archipich <footboll2051@gmail.com>
2025-01-19 02:37:47 +0300

    Add files via upload

---
+++ Sequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startSequence = 5;
+            int endSequence = 103;
+            int stepSequence = 7;
+
+            for (int i = startSequence; i <= endSequence; i += stepSequence)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
