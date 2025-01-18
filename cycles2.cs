fe61c6d1933878833cb91d6e5754a2df61de9f14
Archipich <footboll2051@gmail.com>
2025-01-19 02:16:40 +0300

    Add files via upload

---
+++ cycles2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cycles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codeWord;
+
+            Console.Write("Вы попали в зазеркалье и стоите перед порталом домой\nСкажите кодовое слово чтобы попасть домой:");
+            codeWord = Console.ReadLine();
+
+            while(codeWord != "exit")
+            {
+                Console.Write("Чёрт, похоже не сработало, надо попробовать еще раз, как там это слово... , точно это ");
+                codeWord = Console.ReadLine();
+            }
+            Console.WriteLine("*Вы проснулись*\nФух, это был лишь сон");
+        }
+    }
+}
