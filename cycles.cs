1d4e20c0bf7d221fb036c1ac819a0a1f557e3b84
Archipich <footboll2051@gmail.com>
2025-01-19 01:33:49 +0300

    Add files via upload

---
+++ cycles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int remindersCounters;
+
+            Console.Write("Сколько раз вам нужно напомнить посмотреть курсы? ");
+            remindersCounters = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < remindersCounters; i++)
+            {
+                Console.WriteLine("Обязательно посмотри сегодня курсы");
+            }
+        }
+    }
+}
